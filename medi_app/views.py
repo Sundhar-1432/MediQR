111e031ae0ed70c0a20909ba0dc08fb4c4b3f933
sundhar-1432 <SVSsundhar143@gmail.com>
2026-06-19 00:32:40 +0530

    second update

--- medi_app/views.py
+++ medi_app/views.py
@@ -1,14 +1,14 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-
+from .models import User
 
 def save(request):
     name = request.POST.get('username')
     email = request.POST.get('email')
     password = request.POST.get('password')
-
-    print(name)
-
+    
+    User.objects.create(username=name, email=email, password=password)
+    
     return HttpResponse("Data saved successfully!")
 def register(request):
     return render(request, 'register.html')
